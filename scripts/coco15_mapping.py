864ac193e13b95abaac6b472640cef7eb8399f3d
RajMajhi <rajm46921@gmail.com>
2026-09-17 11:36:30 +0530

    Add COCO 15-class configuration

---
+++ scripts/coco15_mapping.py
@@ -0,0 +1,35 @@
+COCO_TO_TRAIN = {
+    1: 0,    # person
+    2: 1,    # bicycle
+    3: 2,    # car
+    4: 3,    # motorcycle
+    6: 4,    # bus
+    8: 5,    # truck
+    10: 6,   # traffic light
+    11: 7,   # fire hydrant
+    13: 8,   # stop sign
+    15: 9,   # bench
+    62: 10,  # chair
+    63: 11,  # couch
+    64: 12,  # potted plant
+    85: 13,  # clock
+    72: 14,  # tv
+}
+
+CLASS_NAMES = [
+    "person",
+    "bicycle",
+    "car",
+    "motorcycle",
+    "bus",
+    "truck",
+    "traffic light",
+    "fire hydrant",
+    "stop sign",
+    "bench",
+    "chair",
+    "couch",
+    "potted plant",
+    "clock",
+    "tv",
+]
